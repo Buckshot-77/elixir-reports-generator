9d0b8b74f237ec5fdb0c860e1d5a0c107914cf53
Hugo Oliveira <hugo684@gmail.com>
2021-12-25 18:36:27 -0300

    Updated "fetch highest" function to allow searching for user or food

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,13 +1,43 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "hambúrguer",
+    "pastel",
+    "pizza",
+    "prato_feito",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
     |> Enum.reduce(report_accumulator(), fn line, report -> sum_values(line, report) end)
   end
 
-  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
+  def fetch_highest_spender_or_most_sold(report, option),
+    do: Enum.max_by(report[option], fn {_key, value} -> value end)
 
-  defp report_accumulator, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    update_map(report, users, foods)
+  end
+
+  defp update_map(report, users, foods) do
+    report
+    |> Map.put("users", users)
+    |> Map.put("foods", foods)
+  end
+
+  defp report_accumulator do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
+  end
 end
