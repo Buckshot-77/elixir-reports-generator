5b9d3f82de6521107bcc303045e3e90a68e3bf48
Hugo Oliveira <hugo684@gmail.com>
2021-12-25 16:18:48 -0300

    Inclusion of the function sum_values

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,11 +1,13 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
     filename
-    |> ReportsGenerator.Parser.parse_file()
-    |> Enum.reduce(report_accumulator(), fn [id, _food_name, price], report ->
-      Map.put(report, id, report[id] + price)
-    end)
+    |> Parser.parse_file()
+    |> Enum.reduce(report_accumulator(), fn line, report -> sum_values(line, report) end)
   end
+
+  defp sum_values([id, _food_name, price], report), do: Map.put(report, id, report[id] + price)
 
   defp report_accumulator, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
