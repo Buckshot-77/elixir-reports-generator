731e1c3538ef43b07d9594b7eb59bcd6d731f18b
Hugo Oliveira <hugo684@gmail.com>
2021-12-26 14:50:21 -0300

    Built a function which build the reports in parallel

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -20,11 +20,36 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_accumulator(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_from_many(filename_list) do
+    filename_list
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(report_accumulator(), fn {:ok, result}, report ->
+      sum_reports(result, report)
+    end)
+  end
+
   def fetch_highest_spender_or_most_sold(report, option) when option in @options do
     {:ok, Enum.max_by(report[option], fn {_key, value} -> value end)}
   end
 
   def fetch_highest_spender_or_most_sold(_report, _option), do: {:error, "Invalid option!"}
+
+  defp sum_reports(%{"users" => users1, "foods" => foods1}, %{
+         "users" => users2,
+         "foods" => foods2
+       }) do
+    foods = merge_maps(foods1, foods2)
+
+    users = merge_maps(users1, users2)
+
+    build_report(foods, users)
+  end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 ->
+      value1 + value2
+    end)
+  end
 
   defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
     users = Map.put(users, id, users[id] + price)
@@ -43,6 +68,8 @@ defmodule ReportsGenerator do
     foods = Enum.into(@available_foods, %{}, &{&1, 0})
     users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 
-    %{"users" => users, "foods" => foods}
+    build_report(foods, users)
   end
+
+  defp build_report(foods, users), do: %{"foods" => foods, "users" => users}
 end
